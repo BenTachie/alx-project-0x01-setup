078ef31bb4eba57222612601b990a068e7b7290b
BenTachie <tachiebenedict@gmail.com>
2025-07-20 13:05:11 +0200

    fix: implement posts.map to dynamically render UserCard components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -17,7 +17,7 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
           <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {posts?.map((user: UserProps, key: number) => (
+          {posts.map((user: UserProps, key: number) => (
             <UserCard key={key} {...user} />
           ))}
         </div>
