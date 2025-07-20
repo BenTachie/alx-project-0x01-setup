938813569e2629868a5c51f542af2f6a332c25e2
BenTachie <tachiebenedict@gmail.com>
2025-07-20 04:27:31 +0200

    feat: add navigation header and integrate routing between Home, Posts, and Users pages

--- alx-project-0x01/pages/post/index.tsx
+++ alx-project-0x01/pages/post/index.tsx
@@ -1,12 +1,1 @@
-import React from 'react';
-
-const Home: React.FC = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">Welcome to My Website</h1>
-      <p className="mt-4">This is the home page of my awesome website.</p>
-    </div>
-  );
-}
-
-export default Home;
+import Header from "../../components/layout/Header";  
